6b2199c77e6ee0b0e4bd2c01cdbda305d9548b1d
GhostVaibhav <48472541+GhostVaibhav@users.noreply.github.com>
2023-04-03 00:07:50 +0530

    Initial commit

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className="footerParentDiv">
+      <div className="content">
+        <div>
+          <div className="heading">
+            <p>POPULAR LOCATIONS</p>
+          </div>
+          <div className="list">
+            <ul>
+              <li>Kolkata</li>
+              <li>Mumbai</li>
+              <li>Chennai</li>
+              <li>Pune</li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <div className="heading">
+            <p>ABOUT US</p>
+          </div>
+          <div className="list">
+            <ul>
+              <li>Meet the team</li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <div className="heading">
+            <p>VIT Tradestation</p>
+          </div>
+          <div className="list">
+            <ul>
+              <li>Help</li>
+              <li>Sitemap</li>
+              <li>Legal & Privacy information</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="footer">
+        <p>Features coming for other universities soon</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
